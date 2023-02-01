b77ebc112eac9610e912e2a977a99df059c51470
Jenniferv23 <jvanegas369@misena.edu.co>
2023-02-01 16:41:43 -0500

    Se agrega el ejercicio de Strings

---
+++ ejercicio-cadena-de-texto.js
@@ -0,0 +1,19 @@
+
+let nombre = "Jennifer ";
+let apellido = "Vanegas";
+
+console.log(nombre.concat(apellido));
+
+let estudiante = "estudianteMayus";
+console.log(estudiante.toUpperCase());
+
+let estudianteMinus = "estudianteMinus";
+console.log(estudianteMinus.toLowerCase());
+
+console.log(estudiante.length)
+
+console.log(nombre.charAt(0));
+
+console.log(nombre.charAt(7));
+
+console.log(input.toUpperCase());
